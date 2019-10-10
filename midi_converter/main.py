2a4116f4d0be9fef8d9d3a7b2d00c3b4c7429793
rotifyld <dawidborys98@gmail.com>
2019-10-10 19:51:54 +0200

    directory reorganisation

--- midi_converter/main.py
+++ midi_converter/main.py
@@ -1,10 +1,5 @@
-import os
-from pprint import pprint
-from typing import List, Iterable
-
 from logger import log_info
-from song import Song
-from importer import import_all_midis
+from midi_converter.importer import import_all_midis
 
 
 songs = import_all_midis()
